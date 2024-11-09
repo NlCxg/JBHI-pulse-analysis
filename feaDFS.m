4263705d5cd03e9589058063324cce31adddd488
NlCxg <31640593+NlCxg@users.noreply.github.com>
2024-11-09 19:13:51 +0800

    Update feaDFS.m

--- feaDFS.m
+++ feaDFS.m
@@ -1,16 +1,6 @@
 function fea = feaFourier_6(cycle2)
-%CREATEFIT(CYCLE2)
-%  Create a fit.
-%
-%  Data for 'untitled fit 1' fit:
-%      Y Output: cycle2
-%  Output:
-%      fitresult : a fit object representing the fit.
-%      gof : structure with goodness-of fit info.
-%
-%  ÁíÇë²ÎÔÄ FIT, CFIT, SFIT.
 
-%  ÓÉ MATLAB ÓÚ 23-Mar-2017 14:27:03 ×Ô¶¯Éú³É
+
 
 
 %% Fit: 'untitled fit 1'.
@@ -33,13 +23,7 @@ fea=[fitresult.a1 fitresult.b1 ...
        fitresult.a6 fitresult.b6 ...
      fitresult.w];
  
-% Plot fit with data.
-% figure( 'Name', 'untitled fit 1' );
-% h = plot( fitresult, xData, yData );
-% legend( h, 'cycle2', 'untitled fit 1', 'Location', 'NorthEast' );
-% % Label axes
-% ylabel cycle2
-% grid on
+
 
 else
     fea=zeros(1,13);
